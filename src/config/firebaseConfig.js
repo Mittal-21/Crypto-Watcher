7771dbfa940ba1c9efffb0df30da24b478ab4bc4
Rajat <mittal.rajat224555@gmail.com>
2022-06-08 02:41:36 +0530

    First commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBGKFKdjNZM5CwRTmDZGMN4QQp1z8Q_idA",
+    authDomain: "crypto-watcher-d858d.firebaseapp.com",
+    projectId: "crypto-watcher-d858d",
+    storageBucket: "crypto-watcher-d858d.appspot.com",
+    messagingSenderId: "24482194094",
+    appId: "1:24482194094:web:c244f032ae8a4be71c033f"
+  };
+
+  export default firebaseConfig;
